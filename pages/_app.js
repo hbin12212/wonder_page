312d76930c90271b3752c3a3f5405c005b4d1bdc
김효빈 <52522662+hbin12212@users.noreply.github.com>
2022-11-29 15:49:34 +0900

    [FIX] : og tag 추가

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import "../styles/globals.scss";
 import Script from "next/script";
+import Head from "next/head";
 import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }) {
     return (
         <>
+            <Head>
+                <title>Wonder | 어쩌면 우리는 직업 그 이상</title>
+                <meta property="og:title" content="Wonder" />
+                <meta property="og:description" content="어쩌면 우리는 직업 그 이상" />
+                <meta property="og:url" content="https://wonderkr.vercel.app/" />
+                <meta
+                    property="og:image"
+                    content="https://wonderkr.vercel.app/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Ftags.560726e3.png&w=1080&q=75"
+                />
+            </Head>
             <Script
                 strategy="afterInteractive"
                 src={`https://www.googletagmanager.com/gtag/js?id=${gtag?.GA_TRACKING_ID}`}
